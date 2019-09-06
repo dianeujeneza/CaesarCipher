7bb0f202c0c6bed3e6f4a36b5d50f0dd7857ad65
Benedicte-Manishimwe <benoite251@gmail.com>
2019-09-06 17:05:45 +0200

    declare string of encryt class

--- Decrypt.java
+++ Decrypt.java
@@ -1,3 +1,21 @@
-public class Decrypt {
-
-}
+//public class Decrypt {
+//
+//    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+//    public static String decrypt(String cipherText, int shiftKey)
+//    {
+//        cipherText = cipherText.toLowerCase();
+//        String plainText = "";
+//        for (int i = 0; i < cipherText.length(); i++)
+//        {
+//            int charPosition = ALPHABET.indexOf(cipherText.charAt(i));
+//            int keyVal = (charPosition - shiftKey) % 26;
+//            if (keyVal < 0)
+//            {
+//                keyVal = ALPHABET.length() + keyVal;
+//            }
+//            char replaceVal = ALPHABET.charAt(keyVal);
+//            plainText += replaceVal;
+//        }
+//        return plainText;
+//    }
+//}
